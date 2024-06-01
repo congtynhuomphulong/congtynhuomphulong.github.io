50a15e2a273aac6da238f278336c22e443f700c2
congtynhuomphulong <nhuomphulongcompany@gmail.com>
2024-06-01 16:11:00 +0700

    update hardrefresh

--- js/hardRefresh.js
+++ js/hardRefresh.js
@@ -12,4 +12,14 @@ async function handleHardReload(url) {
     window.location.reload();
 }
 
+function urlWithRndQueryParam(url, paramName) {
+    const ulrArr = url.split('#');
+    const urlQry = ulrArr[0].split('?');
+    const usp = new URLSearchParams(urlQry[1] || '');
+    usp.set(paramName || '_z', `${Date.now()}`);
+    urlQry[1] = usp.toString();
+    ulrArr[0] = urlQry.join('?');
+    return ulrArr.join('#');
+}
+
 handleHardReload(window.location.href);
