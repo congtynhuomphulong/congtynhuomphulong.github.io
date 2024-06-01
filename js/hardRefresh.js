38b88cef3c8b33906279164a58413be5266cfca8
congtynhuomphulong <nhuomphulongcompany@gmail.com>
2024-06-01 16:03:37 +0700

    hardfresh2

--- js/hardRefresh.js
+++ js/hardRefresh.js
@@ -1,21 +1,15 @@
-function hardRefresh() {
-    const t = parseInt(Date.now() / 10000); //10s tics
-    const x = localStorage.getItem("t");
-    localStorage.setItem("t", t);
+async function handleHardReload(url) {
+    const newUrl = urlWithRndQueryParam(url);
+    await fetch(newUrl, {
+        headers: {
+            Pragma: 'no-cache',
+            Expires: '-1',
+            'Cache-Control': 'no-cache',
+        },
+    });
+    window.location.href = url;
+    // This is to ensure reload with url's having '#'
+    window.location.reload();
+}
 
-    if (x != t) location.reload(true) //force page refresh from server
-    else { //refreshed from server within 10s
-      const a = document.querySelectorAll("a, link, script, img")
-      var n = a.length
-      while(n--) {
-        var tag = a[n]
-        var url = new URL(tag.href || tag.src);
-        url.searchParams.set('r', t.toString());
-        tag.href = url.toString(); //a, link, ...
-        tag.src = tag.href; //rerun script, refresh img
-      }
-    }
-  }
-
-  window.addEventListener("DOMContentLoaded", hardRefresh);
-  window.addEventListener("deviceorientation", hardRefresh, true);
+handleHardReload(window.location.href);
