1b5a15aa54b4ed270431268136c062bf360acf2c
tianangnho <danielnguyen12a115@gmail.com>
2024-06-05 11:11:40 +0700

    Commit multiple months

--- js/variable.js
+++ js/variable.js
@@ -1,62 +1,63 @@
 var startIndex = 4;
+var oldHolidayMap = new Map();
 
 document.addEventListener("readystatechange", (event) => {
   if (event.target.readyState === "interactive") {
     //nitLoader();
   } else if (event.target.readyState === "complete") {
-	  loadJson();
-	  document.getElementById("bangchamcong").innerHTML = "BẢNG CHẤM CÔNG - Tháng 5 năm 2024";
-	  fillDateInWeek();
-	  fillDateInHoliday();
+    changeMonth();
+    document.getElementById("bangchamcong").innerHTML = "BẢNG CHẤM CÔNG - Tháng 5 năm 2024";
+    fillDateInWeek();
+    //    fillDateInHoliday();
   }
 });
 
-function loadJson(){
-	fetch('./data/data.json', {cache: "reload"})
-      .then((res) => res.text())
-      .then((text) => {
-        //document.getElementById("demo").innerHTML = text;
-        const obj = JSON.parse(text);
-       // document.getElementById("nv1_ho").innerHTML = obj.nv1.fristName;
-        //document.getElementById("nv1_ten").innerHTML = obj.nv1.lastName;
-        for (let i = 1; i < 5; i++) {
-          var nhanVien = "nv" + i;
-          var ho =nhanVien+"_ho";
-          var ten =nhanVien+"_ten";
-          var chucdanh = nhanVien+"_chucdanh";
-          document.getElementById(ho).innerHTML = obj[nhanVien].fristName;
-          document.getElementById(ten).innerHTML = obj[nhanVien].lastName;
-          document.getElementById(chucdanh).innerHTML = obj[nhanVien].job;
-          for (const entry of obj[nhanVien].data) {
-            var date = entry.date;
-            var str = nhanVien + "_vao" + date;
-            document.getElementById(str).innerHTML = entry.in;
-            if (entry.in == "Ko dữ liệu") {
-              document.getElementById(str).style.color = "#ff0000";
-            }
-            var str2 = nhanVien + "_ra" + date;
-            document.getElementById(str2).innerHTML = entry.out;
-            if (entry.out == "Ko dữ liệu") {
-              document.getElementById(str2).style.color = "#ff0000";
-            }
+function loadJson(jsonFile) {
+  fetch(jsonFile, { cache: "reload" })
+    .then((res) => res.text())
+    .then((text) => {
+      //document.getElementById("demo").innerHTML = text;
+      const obj = JSON.parse(text);
+      // document.getElementById("nv1_ho").innerHTML = obj.nv1.fristName;
+      //document.getElementById("nv1_ten").innerHTML = obj.nv1.lastName;
+      for (let i = 1; i < 5; i++) {
+        var nhanVien = "nv" + i;
+        var ho = nhanVien + "_ho";
+        var ten = nhanVien + "_ten";
+        var chucdanh = nhanVien + "_chucdanh";
+        document.getElementById(ho).innerHTML = obj[nhanVien].fristName;
+        document.getElementById(ten).innerHTML = obj[nhanVien].lastName;
+        document.getElementById(chucdanh).innerHTML = obj[nhanVien].job;
+        for (const entry of obj[nhanVien].data) {
+          var date = entry.date;
+          var str = nhanVien + "_vao" + date;
+          document.getElementById(str).innerHTML = entry.in;
+          if (entry.in == "Ko dữ liệu") {
+            document.getElementById(str).style.color = "#ff0000";
+          }
+          var str2 = nhanVien + "_ra" + date;
+          document.getElementById(str2).innerHTML = entry.out;
+          if (entry.out == "Ko dữ liệu") {
+            document.getElementById(str2).style.color = "#ff0000";
           }
         }
+      }
     });
 }
 
 function fillDateInWeek() {
   var elements = document.getElementsByClassName("thuColor");
-  Array.prototype.forEach.call(elements, function(element, index) {
+  Array.prototype.forEach.call(elements, function (element, index) {
     var bumber = 0;
     if (index < 31) {
-      number = (index + startIndex)%7;
+      number = (index + startIndex) % 7;
       if (number == 1) {
         element.innerHTML = "CN";
         element.style.background = "#f161bfe";
-        var str = "date"+(index+1);
+        var str = "date" + (index + 1);
         var els = document.getElementsByClassName(str);
-        Array.prototype.forEach.call(els, function(e) {
-          e.style.background="#ffe680";
+        Array.prototype.forEach.call(els, function (e) {
+          e.style.background = "#ffe680";
         });
       } else if (number == 0) {
         element.innerHTML = "Th7";
@@ -70,7 +71,7 @@ function fillDateInWeek() {
 function fillDateInHoliday() {
   var elements = document.getElementsByClassName("leColor");
   Array.prototype.forEach.call(elements, function (element, index) {
-    if(element.innerHTML == "L"){
+    if (element.innerHTML == "L") {
       element.style.background = "#f161bfe";
       var str = "date" + (index + 1);
       var els = document.getElementsByClassName(str);
@@ -79,4 +80,62 @@ function fillDateInHoliday() {
       });
     }
   });
+}
+
+function fillDateInHoliday(thangNam) {
+  fetch("./js/holiday.json", { cache: "reload" })
+    .then((res) => res.text())
+    .then((text) => {
+      const obj = JSON.parse(text);
+      var holidayMap = new Map();
+      for (year in obj) {
+        var valueY = obj[year];
+        for (month in valueY) {
+          var dates = valueY[month];
+          holidayMap.set(year + month, dates);
+        }
+      }
+
+      for (let [key, value] of oldHolidayMap) {
+        key.style.background = value;
+        if (key.innerHTML == "L") {
+          key.innerHTML = "";
+        }
+      }
+
+      oldHolidayMap.clear();
+      var holidays = holidayMap.get(thangNam);
+      if (typeof (holidays) != "undefined") {
+        const arrayh = holidays.split(",");
+        for (var date of arrayh) {
+          var str = "date" + Number(date);
+          var els = document.getElementsByClassName(str);
+          Array.prototype.forEach.call(els, function (e) {
+            oldHolidayMap.set(e, e.style.background);
+            e.style.background = "#ffe680";
+          });
+          var els2 = document.getElementsByClassName("leColor " + str);
+          Array.prototype.forEach.call(els2, function (e) {
+            e.innerHTML = "L";
+          });
+        }
+      }
+    });
+}
+
+function changeMonth() {
+  var e = document.getElementById("thang");
+  var thangNam = e.value;
+  //var text = e.options[e.selectedIndex].text;
+  var jsonFile = "./data/" + "data_" + thangNam + ".json";
+  var thang = thangNam.substring(5, 6);
+  var nam = thangNam.substring(0, 4)
+  setupStartIndex(thang, nam);
+  fillDateInHoliday(thangNam);
+  loadJson(jsonFile);
+}
+
+function setupStartIndex(thang, nam) {
+  var date = new Date(nam + "-" + thang + "-01");
+  startIndex = date.getDay();
 }
